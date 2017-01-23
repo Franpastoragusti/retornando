9fd7eb196a58b8af02e1398293d33f83622859a5
Franpastoragusti <franciscopa91@gmail.com>
2017-01-23 10:19:31 +0100

    prueba hook con permisos

--- despliegue.php
+++ despliegue.php
@@ -1,15 +1,16 @@
 <?php
 
     if (isset($_POST['payload'])) {
-        
+
         //Obtener los datos de GitHub.
         $payload = json_decode($_POST['payload']);
-        
+
         if ($payload->ref == "refs/heads/master") {
-		
+
             //ejecutar el script del servidor.
-            shell_exec('./deploy.sh');
+            shell_exec('./deploy.sh 2>&1',$output);
         }
+        echo $output;
     }
-    
+
 ?>
